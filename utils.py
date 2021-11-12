5d46e853d55af8db5089e850868bff667bdb14c2
Allard Oelen <oelen@l3s.de>
2021-11-12 16:34:11 +0100

    store in batches

--- utils.py
+++ utils.py
@@ -2,16 +2,27 @@ from mongo import collection_papers, collection_properties, collection_resources
 import multiprocessing as mp
 import time
 
+bulk = collection_papers.initialize_unordered_bulk_op()
+counter = 0
+
 def save_nlp_statements(paper_id, tool_name, data):
-    filter = {"id": paper_id}
-    print('Paper ID', paper_id)
+    global bulk
+    global counter
+
+    print('Paper ID: ', paper_id)
+
     updated_data = {
         "$set": {
             f"nlp_results.{tool_name}": data,
         }
     }
     
-    collection_papers.update_one(filter, updated_data)
+    bulk.find({ 'id': paper_id }).update(updated_data)
+    counter += 1
+
+    if (counter % 500 == 0):
+        bulk.execute()
+        bulk = collection_papers.initialize_ordered_bulk_op()
 
 def make_pool(run_tool, tool_name, overwrite=False, threads=1, field=".*"):
     print(f'Start running {tool_name}, on {threads} threads')
@@ -36,5 +47,8 @@ def make_pool(run_tool, tool_name, overwrite=False, threads=1, field=".*"):
         pool.close()
     
     end = time.time()
+
+    if (counter % 500 != 0):
+        bulk.execute()
 
     print('Finished, time elapsed:', end - start)
